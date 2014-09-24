41204b4113b8a61f540822c2f6e5c69da498ae48
Tommy Salomonsson <tommy@salomonsson.se>
2014-09-24 18:59:37 +0200

    first commit

---
+++ Assets/tommy/StartScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+	
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Application.LoadLevel("Level");
+		}
+	}
+}
